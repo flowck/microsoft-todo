5f2ae89eeb92b0f18cf353484f473f562acc87a5
Firmino Changani <firmino.changani@gmail.com>
2021-09-18 22:45:51 +0100

    feat(#12): add task component

---
+++ src/common/components/Task/Task.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Check from "@/common/icons/check.svg";
+import Favorited from "@/common/icons/favorited.svg";
+import ToFavorite from "@/common/icons/toFavorite.svg";
+import {
+  Container,
+  FavoriteButton,
+  StatusButton,
+} from "@/common/components/Task/TaskStyles";
+
+export function Task() {
+  const [isComplete, setIsComplete] = useState(false);
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  return (
+    <Container>
+      <StatusButton
+        onClick={() => setIsComplete(!isComplete)}
+        className={isComplete ? "status-button--complete" : ""}
+      >
+        {isComplete ? <img src={Check} alt="Status" /> : null}
+      </StatusButton>
+
+      <span>MEIC - Work on my GSI assignment</span>
+
+      <FavoriteButton onClick={() => setIsFavorited(!isFavorited)}>
+        <img src={isFavorited ? Favorited : ToFavorite} alt="favorite" />
+      </FavoriteButton>
+    </Container>
+  );
+}
