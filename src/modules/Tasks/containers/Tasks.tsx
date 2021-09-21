34c8f5b4d70fa8752bc1929666fc67a4bbb93e80
Firmino Changani <firmino.changani@gmail.com>
2021-09-21 14:38:09 +0100

    feat(#21): add memo to check for completed tasks

--- src/modules/Tasks/containers/Tasks.tsx
+++ src/modules/Tasks/containers/Tasks.tsx
@@ -1,41 +1,20 @@
+import { useMemo } from "react";
 import { RootState } from "@/store";
-import styled from "styled-components";
 import { Task } from "@/modules/Tasks/store/task";
-import { View } from "@/common/containers/View/View";
 import { createAction } from "@/common/store/utils";
 import { useDispatch, useSelector } from "react-redux";
 import { UPDATE_TASK } from "@/modules/Tasks/store/actions";
 import { AddTask } from "@/modules/Tasks/components/AddTask/AddTask";
 import { TaskItem } from "@/modules/Tasks/components/TaskItem/TaskItem";
-// import { Tooltip } from "@/common/components/Tooltip/Tooltip";
-// import { ViewOptionsButton } from "../components/ViewOptions/ViewOptionsButton";
 import { ContainerTitle } from "@/common/components/ContainerTitle/ContainerTitle";
 import { ContainerHeader } from "@/common/components/ContainerHeader/ContainerHeader";
-
-const Container = styled(View)`
-  display: flex;
-  flex-direction: column;
-  background-color: #1e1e1e;
-`;
-
-const TaskList = styled.div`
-  flex-grow: 1;
-`;
-
-const CompletedTag = styled.span`
-  padding: 0 5px;
-  margin: 10px 0;
-  font-size: 12px;
-  color: #778bdd;
-  border-radius: 3px;
-  display: inline-block;
-  background-color: #262626;
-`;
+import { Container, TaskList, CompletedTag } from "@/modules/Tasks/containers/TaskStyles";
 
 export function Tasks() {
   const IS_COMPLETE = true;
   const dispatch = useDispatch();
   const tasks = useSelector<RootState, Task[]>(({ tasksModule }) => tasksModule.tasks);
+  const hasCompletedTasks = useMemo(() => tasks.some((task) => task.isComplete), [tasks]);
 
   const onTaskStatusChange = (task: Task) => {
     dispatch(createAction<Task>(UPDATE_TASK, task));
@@ -59,8 +38,13 @@ export function Tasks() {
 
       <TaskList>
         {tasks.length ? renderTasks(!IS_COMPLETE) : null}
-        <CompletedTag>Completed</CompletedTag>
-        {tasks.length ? renderTasks(IS_COMPLETE) : null}
+
+        {hasCompletedTasks ? (
+          <>
+            <CompletedTag>Completed</CompletedTag>
+            {renderTasks(IS_COMPLETE)}
+          </>
+        ) : null}
       </TaskList>
 
       <AddTask />
