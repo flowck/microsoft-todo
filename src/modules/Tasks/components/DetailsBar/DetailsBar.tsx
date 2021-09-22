8db268868078c04f4c73634a35b957acbe5f12a8
Firmino Changani <firmino.changani@gmail.com>
2021-09-22 15:53:06 +0100

    fix(#22): add details bar controls
    - close button
    - remove button
    - created at label

--- src/modules/Tasks/components/DetailsBar/DetailsBar.tsx
+++ src/modules/Tasks/components/DetailsBar/DetailsBar.tsx
@@ -1,5 +1,31 @@
-import { Container } from "@/modules/Tasks/components/DetailsBar/DetailsBarStyles";
+import { RootState } from "@/store";
+import { useSelector } from "react-redux";
+import Trash from "@/common/icons/trash.svg";
+import * as DateUtil from "@/common/utils/dates";
+import { Task } from "@/modules/Tasks/store/task";
+import ArrowClose from "@/common/icons/arrow-close.svg";
+import { Container, DetailsContent, DetailsFooter } from "@/modules/Tasks/components/DetailsBar/DetailsBarStyles";
 
-export function DetailsBar() {
-  return <Container></Container>;
+interface Props {
+  onClose(): void;
+}
+
+export function DetailsBar({ onClose }: Props) {
+  const task = useSelector<RootState, Task | null>(({ tasksModule }) => tasksModule.activeTask);
+  const createdAt = DateUtil.format(task?.createdAt);
+
+  return (
+    <Container>
+      <DetailsContent></DetailsContent>
+      <DetailsFooter>
+        <button type="button" onClick={onClose}>
+          <img src={ArrowClose} alt="Close" />
+        </button>
+        <span>Created {createdAt}</span>
+        <button type="button">
+          <img src={Trash} alt="Remove task" />
+        </button>
+      </DetailsFooter>
+    </Container>
+  );
 }
--- src/modules/Tasks/components/DetailsBar/DetailsBarStyles.tsx
+++ src/modules/Tasks/components/DetailsBar/DetailsBarStyles.tsx
@@ -3,6 +3,34 @@ import styled from "styled-components";
 export const Container = styled.aside`
   width: 100%;
   height: 100%;
+  display: flex;
   padding: 28px 14px;
+  flex-direction: column;
   background-color: #323232;
+`;
+
+export const DetailsContent = styled.div`
+  flex-grow: 1;
+`;
+
+export const DetailsFooter = styled.footer`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  span {
+    font-size: 12px;
+    color: #929292;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+    background-color: transparent;
+
+    img {
+      width: 14px;
+      height: 14px;
+    }
+  }
 `;
