228d33ae56a225047e7de8314f8cd6e9216f4f16
Firmino Changani <firmino.changani@gmail.com>
2021-10-01 02:25:36 +0100

    feat(#44): integrate redux-persist into the existing store

--- src/store.tsx
+++ src/store.tsx
@@ -1,19 +1,22 @@
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { Provider } from "react-redux";
+import storage from "redux-persist/lib/storage";
 import { BaseProps } from "./common/interfaces";
 import { tasksReducer } from "@/modules/Tasks/store/reducer";
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { tasksModuleTransformer } from "./modules/Tasks/store/transforms";
+import { createStore, combineReducers, applyMiddleware, compose, Store, AnyAction } from "redux";
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 /**
  * Assemble all reducers
  */
-const reducers = combineReducers({ tasksModule: tasksReducer });
+const persistConfig = { key: "msTodo", storage, transforms: [tasksModuleTransformer] };
+const rootReducer = persistReducer(persistConfig, combineReducers({ tasksModule: tasksReducer }));
 
-export const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk, logger)));
-
+export const store: Store<any, AnyAction> = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)));
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
-
 export const StoreProvider = ({ children }: BaseProps) => <Provider store={store}>{children}</Provider>;
