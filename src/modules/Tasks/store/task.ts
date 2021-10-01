0ab9d2623a06e4d9d65dcb6b6e320c5158d0702d
Firmino Changani <firmino.changani@gmail.com>
2021-10-01 02:26:14 +0100

    feat(#44): transform rehidrated data up cache restore

--- src/modules/Tasks/store/task.ts
+++ src/modules/Tasks/store/task.ts
@@ -1,3 +1,13 @@
+export interface RawTask {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  content: string;
+  _isComplete: boolean;
+  _isFavorite: boolean;
+  listId: string | null;
+}
+
 export class Task {
   id!: number;
   createdAt!: Date;
@@ -6,12 +16,23 @@ export class Task {
   private _isComplete!: boolean;
   private _isFavorite!: boolean;
 
-  constructor(public content: string, listId = null) {
+  constructor(public content: string, listId: string | null = null) {
     this.id = Date.now();
+    this.listId = listId;
     this.isComplete = false;
     this.createdAt = new Date();
     this.updatedAt = new Date();
-    this.listId = listId;
+  }
+
+  static create(task: RawTask): Task {
+    const _task = new Task(task.content, task.listId);
+    _task.id = task.id;
+    _task.createdAt = task.createdAt;
+    _task.updatedAt = task.updatedAt;
+    _task._isFavorite = task._isFavorite;
+    _task._isComplete = task._isComplete;
+
+    return _task;
   }
 
   private updateUpdatedAt() {
