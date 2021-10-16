805267762575bf292eecb2e115878975594e1a72
Firmino Changani <firmino.changani@gmail.com>
2021-10-16 21:55:42 +0100

    feat(#22): add index to reexport  icons

---
+++ src/common/icons/index.ts
@@ -0,0 +1,19 @@
+import SunIcon from "@/common/icons/sun.svg";
+import BellIcon from "@/common/icons/bell.svg";
+import ClipIcon from "@/common/icons/clip.svg";
+import AddIcon from "@/common/icons/add-task.svg";
+import TrashIcon from "@/common/icons/trash.svg";
+import RepeatIcon from "@/common/icons/repeat.svg";
+import CalendarIcon from "@/common/icons/calendar.svg";
+import ArrowCloseIcon from "@/common/icons/arrow-close.svg";
+
+export const Icons = {
+  TrashIcon,
+  SunIcon,
+  BellIcon,
+  AddIcon,
+  ClipIcon,
+  RepeatIcon,
+  CalendarIcon,
+  ArrowCloseIcon,
+};
