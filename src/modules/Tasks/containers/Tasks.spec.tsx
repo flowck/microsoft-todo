f40b88cadc697b86eba6ce89c74d18f8f5190ce6
Firmino Changani <firmino.changani@gmail.com>
2021-10-17 17:10:14 +0100

    feat(#15): update test case

--- src/modules/Tasks/containers/Tasks.spec.tsx
+++ src/modules/Tasks/containers/Tasks.spec.tsx
@@ -1,12 +1,13 @@
 import { Tasks } from "./Tasks";
 import { StoreProvider } from "@/store";
 import { render, screen, fireEvent } from "@testing-library/react";
+import { TaskContainerTypes } from "@/common/typing";
 
 describe("Tasks.tsx", () => {
   it("Should add a new task", async () => {
     let wrapper = render(
       <StoreProvider>
-        <Tasks />
+        <Tasks title="Tasks" tasksType={TaskContainerTypes.TASKS} />
       </StoreProvider>
     );
     const input = wrapper.getByTestId("addTaskInput");
